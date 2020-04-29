73458d720fda9c26cabd38b5bac73212d8f977cc
Bhushan Pagare <bpagare6@gmail.com>
2020-04-29 21:46:53 +0530

    Updates

---
+++ precache-manifest.c393e41008e803422fafb9bba6e78599.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bf1bad69be1c75075fc5e177610a39",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "cef39a96c2338b81038f",
+    "url": "/Sorting-Visualizer/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d1d5875bfc6e991a2dc8",
+    "url": "/Sorting-Visualizer/static/css/main.bdb02ce5.chunk.css"
+  },
+  {
+    "revision": "cef39a96c2338b81038f",
+    "url": "/Sorting-Visualizer/static/js/2.a750c9a2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Sorting-Visualizer/static/js/2.a750c9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d5875bfc6e991a2dc8",
+    "url": "/Sorting-Visualizer/static/js/main.4a2c2969.chunk.js"
+  },
+  {
+    "revision": "77650ddc79e0d550a433",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.6291256e.js"
+  },
+  {
+    "revision": "379c9a6eb2455a0466855dceb6187dde",
+    "url": "/Sorting-Visualizer/static/media/change_size_speed.379c9a6e.gif"
+  },
+  {
+    "revision": "b4e21d189339593ed372338189d9dc39",
+    "url": "/Sorting-Visualizer/static/media/select_algorithm.b4e21d18.gif"
+  },
+  {
+    "revision": "765206068ea02f3c13108d9ed7f9b6a8",
+    "url": "/Sorting-Visualizer/static/media/visualize.76520606.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Visualizer/precache-manifest.00d47a7501bba360cf5227fcf5baff49.js"
+  "/Sorting-Visualizer/precache-manifest.c393e41008e803422fafb9bba6e78599.js"
 );
 
 self.addEventListener('message', (event) => {
