a695ec765949886beda48d3fb83277c4b7c6d75c
Bhushan Pagare <bpagare6@gmail.com>
2020-04-29 17:18:19 +0530

    README updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Helmet} from "react-helmet";
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Sorting Visualizer</title>
+      </Helmet>
       <SortingVisualizer></SortingVisualizer>
     </div>
   );
